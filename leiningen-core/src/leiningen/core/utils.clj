6c7a1f95ae70d4054932569cd340fac48b63742c
Justin Balthrop <git@justinbalthrop.com>
2012-07-26 19:17:23 -0700

    add resolve-symbol to utils and use it in resolve-task

--- leiningen-core/src/leiningen/core/utils.clj
+++ leiningen-core/src/leiningen/core/utils.clj
@@ -1,10 +1,19 @@
-(ns leiningen.core.utils)
+(ns leiningen.core.utils
+  (:require [clojure.java.io :as io]))
 
 (defn read-file
   "Read the contents of file if it exists."
   [file]
   (when (.exists file)
     (read-string (slurp file))))
+
+(defn ns-exists? [namespace]
+  (some (fn [suffix]
+          (-> (#'clojure.core/root-resource namespace)
+              (subs 1)
+              (str suffix)
+              io/resource))
+        [".clj" (str clojure.lang.RT/LOADER_SUFFIX ".class")]))
 
 (defmacro defdeprecated [old new]
   `(let [new# ~(str (.getName (:ns (meta (resolve new)))) "/" (name new))
@@ -43,3 +52,13 @@
   (binding [*out* *err*]
     (apply println msg)
     (exit 1)))
+
+(defn resolve-symbol
+  "Resolve a fully qualified symbol by first requiring its namespace."
+  [sym]
+  (when-let [ns (namespace sym)]
+    (when (ns-exists? ns)
+      (let [ns (symbol ns)]
+        (when-not (find-ns ns)
+          (require ns)))
+      (resolve sym))))
